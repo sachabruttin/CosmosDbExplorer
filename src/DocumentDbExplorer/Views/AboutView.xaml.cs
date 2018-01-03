ada64e50f8c11aeab2087fc54991e94f4521f065
Sacha Bruttin <sacha.bruttin@b-i.com>
2018-01-03 17:55:08 +0100

    Add External component list in about box

--- src/DocumentDbExplorer/Views/AboutView.xaml.cs
+++ src/DocumentDbExplorer/Views/AboutView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DocumentDbExplorer.Views
 {
@@ -23,6 +12,12 @@ namespace DocumentDbExplorer.Views
         public AboutView()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }

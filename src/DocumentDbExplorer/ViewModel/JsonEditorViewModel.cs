a57512d33d06ddf158c2e0ff8d9ae6c3202eeabb
Sacha Bruttin <sacha.bruttin@b-i.com>
2018-02-12 13:59:06 +0100

    Small fix on Serialization

--- src/DocumentDbExplorer/ViewModel/JsonEditorViewModel.cs
+++ src/DocumentDbExplorer/ViewModel/JsonEditorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
 using DocumentDbExplorer.Infrastructure.Extensions;
 using DocumentDbExplorer.Infrastructure.JsonHelpers;
 using GalaSoft.MvvmLight;

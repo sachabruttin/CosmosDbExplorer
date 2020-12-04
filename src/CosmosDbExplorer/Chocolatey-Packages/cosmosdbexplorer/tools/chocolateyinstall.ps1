6a2a788fce8201959f03be9cfe223d8c54351178
Sacha Bruttin <sacha@bruttin.com>
2020-12-04 13:53:25 +0100

    Update Chocolatey packages

--- src/CosmosDbExplorer/Chocolatey-Packages/cosmosdbexplorer/tools/chocolateyinstall.ps1
+++ src/CosmosDbExplorer/Chocolatey-Packages/cosmosdbexplorer/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com/sachabruttin/CosmosDbExplorer/releases/download/v0.7.5-beta/CosmosDbExplorer.0.7.5.zip'
+$url          = 'https://github.com/sachabruttin/CosmosDbExplorer/releases/download/0.8.0-beta/CosmosDbExplorer.0.8.0.zip'
 $packageName  = $env:ChocolateyPackageName
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir

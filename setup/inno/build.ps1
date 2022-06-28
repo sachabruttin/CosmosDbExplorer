7a1559a52debd9a7c9d5f837834c77317185db00
Sacha Bruttin <sacha@bruttin.com>
2022-06-28 14:31:09 +0200

    Update build.ps1

--- setup/inno/build.ps1
+++ setup/inno/build.ps1
@@ -11,16 +11,16 @@ New-Item -Path "." -Name "bin" -ItemType Directory -Force
 # download the files and pack them
 
 $download = "https://github.com/sachabruttin/CosmosDbExplorer/releases/download/v$Version/CosmosDbExplorer.zip" 
-Invoke-WebRequest $download -Out "./bin/CosmosDbExplorer.zip"
-Expand-Archive -LiteralPath "./bin/CosmosDbExplorer.zip" -DestinationPath "./bin/files" -Force
+Invoke-WebRequest $download -Out ".\bin\CosmosDbExplorer.zip"
+Expand-Archive -LiteralPath ".\bin\CosmosDbExplorer.zip" -DestinationPath ".\bin\files" -Force
 
 $content = Get-Content './setup.iss' -Raw
 $content = $content.Replace('<VERSION>', $Version)
-$ISSName = "./cosmosdbexplorer-$Version.iss"
+$ISSName = ".\cosmosdbexplorer-$Version.iss"
 $content | Out-File -Encoding 'UTF8' $ISSName
 # package content
 $installer = "install"
 ISCC.exe /F$installer $ISSName
 # get hash
-$zipHash = Get-FileHash "Output/$installer.exe" -Algorithm SHA256
-$zipHash.Hash | Out-File -Encoding 'UTF8' "Output/$installer.exe.sha256"
+$zipHash = Get-FileHash "Output\$installer.exe" -Algorithm SHA256
+$zipHash.Hash | Out-File -Encoding 'UTF8' "Output\$installer.exe.sha256"

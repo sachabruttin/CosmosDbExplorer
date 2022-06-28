fac1feb482686e97d92d250b6d4123a9e7d86dda
Sacha Bruttin <sacha@bruttin.com>
2022-06-28 15:30:37 +0200

    Add winget setup CD (#83)

--- setup/inno/build.ps1
+++ setup/inno/build.ps1
@@ -7,21 +7,19 @@ Param
 )
 
 New-Item -Path "." -Name "bin" -ItemType Directory -Force
-
 # download the files and pack them
-@{file = "CosmosDbExplorer.zip"; name = "CosmosDbExplorer.zip" } | ForEach-Object -Process {
-    $download = "https://github.com/sachabruttin/CosmosDbExplorer/releases/download/v$Version/$($_.file)" 
-    Invoke-WebRequest $download -Out "./bin/$($_.name)"
-    Expand-Archive -LiteralPath "./bin/$($_.name)" -DestinationPath "./bin/files" -Force
-}
+
+$download = "https://github.com/sachabruttin/CosmosDbExplorer/releases/download/v$Version/CosmosDbExplorer.zip" 
+Invoke-WebRequest $download -Out ".\bin\CosmosDbExplorer.zip"
+Expand-Archive -LiteralPath ".\bin\CosmosDbExplorer.zip" -DestinationPath ".\bin\files" -Force
 
 $content = Get-Content './setup.iss' -Raw
 $content = $content.Replace('<VERSION>', $Version)
-$ISSName = "./cosmosdbexplorer-$Version.iss"
+$ISSName = ".\cosmosdbexplorer-$Version.iss"
 $content | Out-File -Encoding 'UTF8' $ISSName
 # package content
-$installer = "install"
+$installer = "install-test"
 ISCC.exe /F$installer $ISSName
 # get hash
-$zipHash = Get-FileHash "Output/$installer.exe" -Algorithm SHA256
-$zipHash.Hash | Out-File -Encoding 'UTF8' "Output/$installer.exe.sha256"
+$zipHash = Get-FileHash "Output\$installer.exe" -Algorithm SHA256
+$zipHash.Hash | Out-File -Encoding 'UTF8' "Output\$installer.exe.sha256"

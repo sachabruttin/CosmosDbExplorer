21d71b53e34b94fe3cec543f16148454ddc73d65
Sacha Bruttin <sacha@bruttin.com>
2022-06-30 16:27:01 +0200

    Support query that don't have ContinuationToken

--- src/CosmosDbExplorer.Core/Helpers/FeedResponseExtensions.cs
+++ src/CosmosDbExplorer.Core/Helpers/FeedResponseExtensions.cs
@@ -14,7 +14,7 @@ namespace CosmosDbExplorer.Core.Helpers
             {
                 return feedResponse.ContinuationToken;
             }
-            catch (ArgumentException ex) when (ex.Message == "Continuation token is not supported for queries with GROUP BY. Do not use FeedResponse.ResponseContinuation or remove the GROUP BY from the query.")
+            catch
             {
                 // Silently catch exception 
                 return null;

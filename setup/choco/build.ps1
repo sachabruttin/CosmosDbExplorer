95b22a784312d20b64b7b7cbd7312c1bf796bca7
Sacha Bruttin <sacha@bruttin.com>
2023-05-24 14:11:40 +0200

    Rework chocolatey deployment script

---
+++ setup/choco/build.ps1
@@ -0,0 +1,70 @@
+
+Param
+(
+    [parameter(Mandatory = $true)]
+    [string]
+    $Version,
+    [parameter(Mandatory = $false)]
+    [string]
+    $Token
+)
+
+function Get-HashForArchitecture {
+    param (
+        [parameter(Mandatory = $true)]
+        [string]
+        $Version
+    )
+    $hash = (new-object Net.WebClient).DownloadString("https://github.com/sachabruttin/CosmosDbExplorer/releases/download/v$Version/install.exe.sha256")
+    return $hash.Trim()
+}
+
+function Write-MetaData {
+    param (
+        [parameter(Mandatory = $true)]
+        [string]
+        $FileName,
+        [parameter(Mandatory = $true)]
+        [string]
+        $Version,
+        [parameter(Mandatory = $true)]
+        [string]
+        $Path,
+        [parameter(Mandatory = $true)]
+        [string]
+        $Hash
+    )
+
+    $outPath = $Path -eq 'templates' ? "./output/$FileName" : "./output/$Path/$FileName" 
+    $filePath = $Path -like 'templates' ? "./templates/$FileName" : "./templates/$Path/$FileName"
+
+    $tagName = "v$Version"
+    $Version -match  '(?<version>\d.\d.\d)(?:-)(?<beta>.*)'
+
+    $content = Get-Content $filePath -Raw
+    $content = $content.Replace('$version', $Matches.version)
+    $content = $content.Replace('$tag-name', $tagName)
+    $content = $content.Replace('<HASH>', $Hash)
+    $date = Get-Date -Format "yyyy-MM-dd"
+    $content = $content.Replace('<DATE>', $date)
+    $content | Out-File -Encoding 'UTF8' $outPath
+}
+
+New-Item -Path $PWD -Name "output/tools" -ItemType "directory"
+# Get all files inside the folder and adjust the version/hash
+$Hash = Get-HashForArchitecture  -Version $Version
+
+Get-ChildItem './templates/*' -Recurse -File | ForEach-Object -Process {
+    Write-MetaData -FileName $_.Name -Version $Version -Hash $Hash -Path $_.Directory.BaseName
+}
+
+if (-not $Token) {
+    return
+}
+
+# package
+choco pack "./output/cosmosdbexplorer.nuspec" --out "./output" --allow-unofficial
+
+# push
+$nupkg = (Get-ChildItem '*.nupkg' -Recurse -File).FullName
+choco push $nupkg --api-key=$Token --allow-unofficial
